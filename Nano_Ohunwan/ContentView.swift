f8edc07206c62b3953d6f7411ad058e0cae10721
GODNOEL <103012104+GODNOEL@users.noreply.github.com>
2022-04-29 17:30:27 +0900

    Initial Commit

---
+++ Nano_Ohunwan/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Nano_Ohunwan
+//
+//  Created by Yu ahyeon on 2022/04/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nano_Ohunwan/Nano_OhunwanApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_OhunwanApp.swift
+//  Nano_Ohunwan
+//
+//  Created by Yu ahyeon on 2022/04/29.
+//
+
+import SwiftUI
+
+@main
+struct Nano_OhunwanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
